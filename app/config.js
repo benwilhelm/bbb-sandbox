1b480f18d067d393c985c19d63c924606af6a47a
Ben Wilhelm <ben@doublebeamdesign.com>
2013-05-24 16:56:04 -0500

    First supposedly functional. Page loads with no 404s

---
+++ app/config.js
@@ -0,0 +1,37 @@
+// Set the require.js configuration for your application.
+require.config({
+
+  // Initialize the application with the main application file and the JamJS
+  // generated configuration file.
+  deps: ["../vendor/jam/require.config", "main"],
+
+  paths: {
+    
+    // Javascript Folders
+    libs: "../vendor/js/libs",
+    plugins: "../vendor/js/plugins",
+    
+    // Libraries
+    jquery: "../vendor/jam/jquery/dist/jquery.min",
+    underscore: "../vendor/jam/underscore/underscore",
+    backbone: "../vendor/jam/backbone/backbone"
+  },
+
+  shim: {
+    use: "../vendor/js/plugins/"
+  },
+  
+  use: {
+    "underscore": {
+      attach: "_"
+    },
+    
+    "backbone": {
+      deps: ["use!underscore","jquery"],
+      attach: function(_, $) {
+        return Backbone;
+      }
+    }
+  }
+
+});
